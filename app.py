db791000e67d650dd9d718e4df74e3c4f7cb2390
Sathish Kumar <sathishneo@users.noreply.github.com>
2019-04-13 22:09:29 +0530

    app files to learn python premitives

---
+++ app.py
@@ -0,0 +1,11 @@
+print('hello world')
+print('o----')
+print(' ||||')
+print('#' * 10)
+
+#display heart
+
+print(' ' + '*' * 2 + ' ' + '*' * 2)
+print('*' + ' ' * 2 + '*' + ' ' * 2 + '*')
+print(' ' + '*' + ' ' * 3 + '*')
+print(' ' * 3 + '*')
---
+++ app1.py
@@ -0,0 +1,8 @@
+price = 10
+rating = 4.9
+print(rating)
+name = 'sathish'
+print(name)
+is_published = False
+print(is_published)
+print(price)
