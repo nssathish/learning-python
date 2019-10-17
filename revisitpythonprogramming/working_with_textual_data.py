0a313dd8aa861a870fb7388f2c713d2fb6b12c3a
sathishneo <neosathishgct@gmail.com>
2019-10-17 21:24:28 +0530

    textual data manipulation

---
+++ revisitpythonprogramming/working_with_textual_data.py
@@ -0,0 +1,18 @@
+print("Hello Sathish\'s computer")
+
+message = 'Helloworld'
+print (message[0:5])
+
+for _ in range(0, 5):
+    print(message[_])
+
+for i in range(0,5):
+    print(message[i])
+
+
+print(message[6:])
+
+#string count - word count - letter count etc.,
+print(message.count("Hello"))
+
+print(message.count("l"))
